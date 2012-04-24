d48145b2e38aa03b63b15a0f4175003fa1aa6cc6
Andrew Mulhern <ajm329@drexel.edu>
2012-04-24 15:02:12 -0400

    Updated FB login/logout

--- FacebookLogin/examples/login.php
+++ FacebookLogin/examples/login.php
@@ -20,7 +20,7 @@ require '../src/facebook.php';
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
   'appId'  => '223093184459540',
-  'secret' => '7676231dddc69e4fb46b964e24d5158a',
+  'secret' => '',
 ));
 
 
